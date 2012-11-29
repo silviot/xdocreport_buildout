291475292276dad79f33eeedeb0b8feb6d0f5938
Silvio Tomatis <silviot@gmail.com>
2012-11-29 17:25:56 +0100

    Actually name the class we want to launch

--- convert.py
+++ convert.py
@@ -8,4 +8,5 @@ LIBDIR = os.path.join(os.path.dirname(__file__), 'parts', 'xdocreport_tools', 'l
 jars = []
 for filename in os.listdir(LIBDIR):
     jars.append(os.path.abspath(os.path.join(LIBDIR, filename)))
-subprocess.call(['java', '-classpath', ';'.join(jars)] + sys.argv[1:])
+base_args = ['java', '-classpath', ':'.join(jars), 'fr.opensagres.xdocreport.document.tools.Main' ]
+subprocess.call(base_args + sys.argv[1:])
